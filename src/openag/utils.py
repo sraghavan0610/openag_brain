6ee7be73f13eb6ca0c84c32d27ecce5f7525274a
Leon Chambers <leon8556@gmail.com>
2017-01-27 15:36:28 -0500

    Merged utils files

--- src/openag/utils.py
+++ src/openag/utils.py
@@ -1,7 +1,11 @@
-from openag.categories import SENSORS, ACTUATORS, all_categories
 import os
 import re
+import rospkg
+from re import match
+from sys import maxsize
+from random import randint
 from urlparse import urlparse
+from openag.categories import SENSORS, ACTUATORS, all_categories
 
 def synthesize_firmware_module_info(modules, module_types):
     """
@@ -218,3 +222,40 @@ def safe_cpp_var(s):
     # Prefix with underscore if what is left is a reserved word
     s = "_" + s if s in CPP_KEYWORDS or s[0].isdigit() else s
     return s
+
+def resolve_fixtures(fixtures):
+    """
+    Given a list of fixture names, returns a list of fully resolved fixture
+    paths.
+    """
+    rospack = rospkg.RosPack()
+    pkg_path = rospack.get_path('openag_brain')
+    fixtures_path = os.path.join(pkg_path, "fixtures")
+    resolved = [os.path.join(fixtures_path, fixture_name + ".json") for fixture_name in fixtures]
+    return resolved
+
+def gen_doc_id(curr_time):
+    """
+    Given a unix time, generate a unique ID string with extremely low chance
+    of collision.
+
+    Returns a string.
+    """
+    return "{}-{}".format(curr_time, randint(0, maxsize))
+
+def read_environment_from_ns(namespace):
+    """
+    Given a ROS topic name, attempt to match an environment ID in topic
+    namespace.
+
+    Use:
+
+        read_environment_from_ns(rospy.get_namespace())
+    """
+    result = match("/environments/(\w+)/", namespace)
+    if not result:
+        raise ValueError(
+            "No environment id found in namespace \"{}\".".format(namespace)
+        )
+    environment_id = result.group(1)
+    return environment_id
