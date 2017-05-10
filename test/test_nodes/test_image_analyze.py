3682f1f4e8c3ee5e51607bb082cdd6264b0328a4
Gordon <gordonbrander@users.noreply.github.com>
2017-05-10 15:54:50 -0600

    modified to run

--- test/test_nodes/test_image_analyze.py
+++ test/test_nodes/test_image_analyze.py
@@ -18,30 +18,39 @@ class TestImageAnalyze(unittest.TestCase):
     """
     def setUp(self):
         self.namespace = "tests"
+        self.image_received = False
 
     def callback(self, item):
+        print("===================Image Received--------------------")
+        self.image_received = True
         rospy.loginfo("Image Received {} {}".format(item.width, item.height))
-        # self.assertTrue()
         image_format = self.image_format_mapping.get(item.encoding, None)
         if image_format is None:
             raise ValueError()
         img = Image.fromstring(
             image_format, (item.width, item.height), item.data
         )
-        img.save(get_temp_file_name())
-        
+        file_name = get_temp_file_name()
+        img.save(file_name)
+        print("Image saved to ".format(file_name))
+        self.assertTrue(item.width == 480)
+        self.assertTrue(item.height == 640)
+        exit()
+
     def test_usb_cam(self):
         rospy.init_node(NAME, anonymous=True)
-        camera_source = "aerial_image"
+        camera_source = "test_image"
         self.sub_measured = rospy.Subscriber("{}/{}/image_raw".format(self.namespace, camera_source),
                                             Float64, self.callback)
-        # rospy.sleep(15)  # Wait for subscriber to be ready to receive messages
+        for _ in range(15,0,-1):
+           rospy.loginfo("Waiting {} seconds to receive image.".format(_))
+           rospy.sleep(1)  # Wait for subscriber to be ready to receive messages
         # for msg in self.msgs:
         #     rospy.loginfo("Published: {:f}".format(msg))
         #     self.pub_desired.publish(msg)
         # rospy.sleep(15)  # Wait until all messages are received.
-
-        self.assertTrue(self.msgs == self._received)
+        #rospy.spin()
+        self.assertTrue(self.image_received)
 
 
 if __name__ == "__main__":
