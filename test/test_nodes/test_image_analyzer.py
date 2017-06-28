6d309b034e8399e00fa205204b63bd16c3cee5dd
Bob Davis <bob.davis@fenome.com>
2017-06-27 19:26:06 -0600

    Created working test for usb_cam and cv_bridge

--- test/test_nodes/test_image_analyzer.py
+++ test/test_nodes/test_image_analyzer.py
@@ -6,42 +6,56 @@ import sys
 import unittest
 import tempfile
 import rospy
-from std_msgs.msg import Float64
+import numpy as np
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 def get_temp_file_name():
-    return tempfile.gettempdir() + '/' + next(tempfile._get_candidate_names())
+    return tempfile.gettempdir() + '/' + next(tempfile._get_candidate_names()) + ".jpg"
 
-class TestImageAnalyzer(unittest.TestCase):
+class TestUSBCam(unittest.TestCase):
     """
     Test the imaeg analyzer node. It should read an image from usb_cam and
     process it through the src/openag_cv modules.
     """
+    image_format_mapping = {
+        "rgb8": "RGB",
+        "rgba8": "RGBA"
+    }
+    bridge = CvBridge()
+
     def setUp(self):
-        self.namespace = "tests"
+        self.namespace = "usb_cam"
         self.image_received = False
 
     def callback(self, item):
-        print("===================Image Received--------------------")
+        rospy.loginfo("===================Image Received===================")
         self.image_received = True
         rospy.loginfo("Image Received {} {}".format(item.width, item.height))
         image_format = self.image_format_mapping.get(item.encoding, None)
         if image_format is None:
             raise ValueError()
-        img = Image.fromstring(
-            image_format, (item.width, item.height), item.data
-        )
+        cv2_img = self.bridge.imgmsg_to_cv2(item, "bgr8")
+        #np_arr = np.fromstring(item.data, np.uint8)
+        #image_np = cv2.imdecode(np_arr,cv2.IMREAD_COLOR)
         file_name = get_temp_file_name()
-        img.save(file_name)
-        print("Image saved to ".format(file_name))
-        self.assertTrue(item.width == 480)
-        self.assertTrue(item.height == 640)
+        rospy.loginfo("Saving image to {}".format(file_name))
+        cv2.imwrite(file_name, cv2_img)
+        # rospy.loginfo("Image saved to {}".format(file_name))
+        rospy.loginfo("width: {} height: {}".format(item.width, item.height))
+        self.assertTrue(item.width == 640)
+        self.assertTrue(item.height == 480)
+        self.image_processed = True
 
     def test_usb_cam(self):
+        self.image_received = False
         rospy.init_node(NAME, anonymous=True)
-        camera_source = "test_image"
-        self.sub_measured = rospy.Subscriber("{}/{}/image_raw".format(self.namespace, camera_source),
-                                            Float64, self.callback)
-        for _ in range(15,0,-1):
+        #camera_source = "test_image"
+        image_rostopic = "{}/image_raw".format(self.namespace)
+        self.sub_measured = rospy.Subscriber(image_rostopic, Image, self.callback)
+        rospy.loginfo("Looking for image at " + image_rostopic)
+        for _ in range(3, 0, -1):
            rospy.loginfo("Waiting {} seconds to receive image.".format(_))
            rospy.sleep(1)  # Wait for subscriber to be ready to receive messages
         # for msg in self.msgs:
@@ -50,8 +64,8 @@ class TestImageAnalyzer(unittest.TestCase):
         # rospy.sleep(15)  # Wait until all messages are received.
         #rospy.spin()
         self.assertTrue(self.image_received)
-
+        self.assertTrue(self.image_processed)
 
 if __name__ == "__main__":
     import rostest
-    rostest.rosrun(PKG, NAME, TestImageAnalyzer)
+    rostest.rosrun(PKG, NAME, TestUSBCam)
