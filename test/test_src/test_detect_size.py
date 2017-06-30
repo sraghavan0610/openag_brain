fb10ee890c346c24387391a4faf8bafdb519c1fa
Bob Davis <bob.davis@fenome.com>
2017-06-30 10:19:08 -0600

    updated indents to spaces

--- test/test_src/test_detect_size.py
+++ test/test_src/test_detect_size.py
@@ -27,17 +27,17 @@ def test_midpoint():
 
 #@pytest.mark.usefixture("frame")
 def test_creating_image_mask():
-	pass	
+    pass
 
 #@pytest.mark.usefixture("frame")
 def test_process_image(frame):
     print(TEST_DIR)
     print(frame.shape)
-	#expected_image = cv2.imread('plant_4_result.jpg')
+    #expected_image = cv2.imread('plant_4_result.jpg')
     detected_size_data = process_image(frame)
     actual_image = detected_size_data[0]
     cv2.imwrite(os.path.join(TEST_DIR, "plant_4_detected_size.jpg"), actual_image)
-	#assert actual_image == output_detect_size_img
+    #assert actual_image == output_detect_size_img
 @pytest.fixture
 def detect_size():
     file_path = os.path.join(TEST_DIR, "plant_4_detected_size.jpg")
@@ -50,7 +50,7 @@ def detect_size():
 def test_process_image(detect_size):
     print(TEST_DIR)
     print(detect_size.shape)
-	#expected_image = cv2.imread('plant_4_result.jpg')
+    #expected_image = cv2.imread('plant_4_result.jpg')
     actual_detected_size_data = process_image(detect_size)
     expected_image = actual_detected_size_data[0]
     cv2.imwrite(os.path.join(TEST_DIR, "actual_plant_4_detected_size.jpg"), expected_image)    
